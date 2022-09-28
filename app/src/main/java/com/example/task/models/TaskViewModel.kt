beddf908dbc40d4dc15e6b99c54d5e87c02ef020
jay <jaymaurya1256@gmail.com>
2022-09-28 23:58:44 +0530

    Implemented local database through room.

--- app/src/main/java/com/example/task/models/TaskViewModel.kt
+++ app/src/main/java/com/example/task/models/TaskViewModel.kt
@@ -1,12 +1,6 @@
 package com.example.task.models
 
-import android.content.Context
 import androidx.lifecycle.ViewModel
-import androidx.room.Room
-import com.example.task.database.Task
-import com.example.task.database.TaskDatabase
 
 class TaskViewModel: ViewModel() {
-    private var _database:TaskDatabase = TaskDatabase.getDataBaseInstance()
-    val database get() = _database
 }
