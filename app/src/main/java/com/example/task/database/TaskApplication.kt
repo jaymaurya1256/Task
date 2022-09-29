424f026b7f208acec1691e16e2a8badba1ccc713
jay <jaymaurya1256@gmail.com>
2022-09-29 11:25:59 +0530

    Implemented local database through room.

---
+++ app/src/main/java/com/example/task/database/TaskApplication.kt
@@ -0,0 +1,7 @@
+package com.example.task.database
+
+import android.app.Application
+
+class TaskApplication: Application() {
+    //val database:TaskDatabase = TaskDatabase.getDataBaseInstance(this)
+}
