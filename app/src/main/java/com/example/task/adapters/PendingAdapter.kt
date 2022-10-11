392e9afe7e933fb9c2bd6eb687c9db1724d6b8cd
jay <jaymaurya1256@gmail.com>
2022-10-11 14:51:25 +0530

    Added menu options when user tap and holds on task

--- app/src/main/java/com/example/task/adapters/PendingAdapter.kt
+++ app/src/main/java/com/example/task/adapters/PendingAdapter.kt
@@ -4,9 +4,11 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.PopupMenu
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
+import com.example.task.MainActivity
 import com.example.task.R
 import com.example.task.database.Task
 import com.example.task.models.TaskViewModel
@@ -28,6 +30,22 @@ class PendingAdapter(private val taskList: List<Task>,private val lambdaMarkComp
             //lambda implementation should be there i.e sharedViewModel should not be used here
             lambdaMarkCompleted.invoke().markCompleted(taskList[position])
         }
+        holder.listItem.setOnLongClickListener(object : View.OnLongClickListener{
+            override fun onLongClick(p0: View?): Boolean {
+                val popupMenu: PopupMenu = PopupMenu(p0?.context,p0)
+                popupMenu.menuInflater.inflate(R.menu.item_list_menu,popupMenu.menu)
+                popupMenu.setOnMenuItemClickListener { it ->
+                    when(it.itemId){
+                        R.id.delete -> TODO()
+                        R.id.edit -> TODO()
+                        else -> TODO()
+                    }
+                }
+
+                popupMenu.show()
+                return true
+            }
+        })
     }
 
     override fun getItemCount(): Int {
